b076b803d5484e6bb806ce7daa3a382bfcea9726
Bruno <you@example.comkingfcross@gmail.com>
2019-07-18 18:16:12 -0300

    Ingress Controller Created

--- PRODUCTPAGE.py
+++ PRODUCTPAGE.py
@@ -46,4 +46,63 @@ spec:
         ports:
         - containerPort: 9080
 ---
+apiVersion: extensions/v1
+kind: Deployment
+metadata: 
+  name: nginx-ingress-controller
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      name: nginx-ingress
+  template:
+    metadata:
+      labels:
+        name: nginx-ingress
+    spec:
+      containers:
+        - name: nginx-ingress-controller
+          image: quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.21.0
+      args:
+        - /nginx-ingress-controller
+        - --configmap=$(POD_REVIEWS)/nginx-oconfiguration
+      env:
+        - name: POD_REVIEW
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.productpage
+        - name: POD_REVIEW
+          valueFrom:
+            fieldPath: metadata.productpage
+      ports:
+        - name: http
+          containerPort: 9080
+        - name: https
+          containerPort: 443
+apiVersion: v1
+kind: service
+metadata: 
+  name: nginx-ingress
+spec:
+  type: NodePort
+  ports:
+  - port: 9080
+    targetPort: 9080
+    protocol: TCP
+    name: http
+  - port: 443
+    targetPort: 443
+    protocol: TCP
+    name: https
+  selector:
+    name:nginx-ingress
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: nginx-ingress-serviceaccount
 
+kind: ConfigMap
+apiVersion:v1
+metadata:
+  name: nginx-configuration
+-
